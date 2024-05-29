f348aea1579be9add8051d98ffe6e8684a851dc6
Yelyzaveta Rohovska <lizarogovskaia@gmail.com>
2024-05-29 10:23:19 +0200

    [B] Setup an express server and database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,16 @@
-import express from 'express'
-import { db } from './database'
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const port = 8000;
 
 const app = express()
-const port = 8000
 
-app.get('/', (_req, res) => {
-  res.status(200).json({ message: 'Hello, World!' })
-})
-
-app.get('/users', (_req, res) => {
-    const users = db.getUsers()
-    res.status(200).json(users)
-})
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+  console.log(`Server is running on http://localhost:${port}`);
 })
 
--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,111 +1,119 @@
 // Dummy database
 
-const users = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@doe.com'
-    },
-    {
-        id: 2,
-        name: 'Jane Doe',
-        email: 'jane@doe.com'
-   },
-   {
-       id: 3,
-       name: 'Alice Brown',
-       email: 'alice@brown.com'
-   },
-   {
-       id: 4,
-       name: 'Bob Brown',
-       email: 'bob@brown.com'
-   }
-]
+import { ECategory } from "./models/types";
 
-const activities = [
-    {
-        id: 1,
-        name: 'Sale',
-        points: 10
-    },
-    {
-        id: 2,
-        name: 'Meeting',
-        points: 5
-    },
-    {
-        id: 3,
-        name: 'Valuation',
-        points: 3
-    }
-]
+const users = [{
+    id: 1,
+    name: 'John Doe',
+    email: 'john@doe.com'
+},
+{
+    id: 2,
+    name: 'Jane Doe',
+    email: 'jane@doe.com'
+},
+{
+   id: 3,
+   name: 'Alice Brown',
+   email: 'alice@brown.com'
+},
+{
+   id: 4,
+   name: 'Bob Brown',
+   email: 'bob@brown.com'
+}];
 
-const userActivities = [
-    {
-        id: 1,
-        userId: 1,
-        activityId: 1,
-        timestamp: '2021-01-01T00:00:00'
-    },
-    {
-        id: 2,
-        userId: 1,
-        activityId: 2,
-        timestamp: '2021-01-02T00:00:00'
-    },
-    {
-        id: 3,
-        userId: 2,
-        activityId: 1,
-        timestamp: '2021-01-03T00:00:00'
-    },
-    {
-        id: 4,
-        userId: 2,
-        activityId: 2,
-        timestamp: '2021-01-04T00:00:00'
-    },
-    {
-        id: 5,
-        userId: 3,
-        activityId: 1,
-        timestamp: '2021-01-05T00:00:00'
-    },
-    {
-        id: 6,
-        userId: 3,
-        activityId: 2,
-        timestamp: '2021-01-06T00:00:00'
-    },
-    {
-        id: 7,
-        userId: 4,
-        activityId: 1,
-        timestamp: '2021-01-07T00:00:00'
-    },
-    {
-        id: 8,
-        userId: 4,
-        activityId: 2,
-        timestamp: '2021-01-08T00:00:00'
-    }
-]
+const activities = [{
+    id: 1,
+    name: ECategory.sale,
+    points: 10,
+},
+{
+    id: 2,
+    name: ECategory.meeting,
+    points: 5,
+},
+{
+    id: 3,
+    name: ECategory.valuation,
+    points: 3,
+}];
 
+const userActivities = [ {
+    id: 1,
+    userId: 1,
+    activityId: 1,
+    timestamp: new Date('2021-01-01T00:00:00'),
+},
+{
+    id: 2,
+    userId: 1,
+    activityId: 2,
+    timestamp: new Date('2021-01-02T00:00:00'),
+},
+{
+    id: 3,
+    userId: 2,
+    activityId: 3,
+    timestamp: new Date('2021-01-03T00:00:00'),
+},
+{
+    id: 4,
+    userId: 2,
+    activityId: 2,
+    timestamp: new Date('2021-01-04T00:00:00'),
+},
+{
+    id: 5,
+    userId: 3,
+    activityId: 3,
+    timestamp: new Date('2021-01-05T00:00:00'),
+},
+{
+    id: 6,
+    userId: 3,
+    activityId: 2,
+    timestamp: new Date('2021-01-06T00:00:00'),
+},
+{
+    id: 7,
+    userId: 4,
+    activityId: 1,
+    timestamp: new Date('2021-01-07T00:00:00'),
+},
+{
+    id: 8,
+    userId: 4,
+    activityId: 3,
+    timestamp: new Date('2021-01-08T00:00:00'),
+}];
 
-class Database {
-    public getUsers() {
-        return users
+class Database<T> {
+    private data: T[];
+
+    constructor(data: T[]) {
+        this.data = data;
     }
 
-    public getActivities() {
-        return activities
-    }
+    find(query: Partial<T> = {}): T[] {
+        return this.data.filter(doc => compareProperties(doc, query)) || [];
+    };
 
-    public getUserActivities() {
-        return userActivities
+    create(doc: Omit<T, "id">): number {
+        return this.data.push({
+            id: this.data.length + 1,
+            ...doc,
+        } as T);
     }
 }
 
-export const db = new Database()
+function compareProperties<T>(obj: T, query: Partial<T>): boolean {
+    return Object.entries(query).every(([key, value]) => obj[key as keyof T] === value);
+}
+
+export const db = {
+    users: new Database(users),
+    activities: new Database(activities),
+    userActivities: new Database(userActivities),
+}
 
